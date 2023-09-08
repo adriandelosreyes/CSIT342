8f31db31b6acfaf0726d3858c56c85fce03cdeea
adriandelosreyes <93171820+adriandelosreyes@users.noreply.github.com>
2023-09-08 14:43:48 +0800

    Add files via upload

---
+++ Assignment-01_DelosReyes/todo.js
@@ -0,0 +1,15 @@
+// Create a new list item when clicking on the "Add" button
+function addListItem() {
+    var li = document.createElement("li");
+    var inputValue = document.getElementById("task-input").value;
+    var t = document.createTextNode(inputValue);
+    li.appendChild(t);
+  
+    if (inputValue === "") {
+      alert("You must write something!");
+    } else {
+      document.getElementById("todo-list").appendChild(li);
+    }
+  
+    document.getElementById("task-input").value = "";
+  }
